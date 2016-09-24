70fd3dc6fd6fa6d4356a48d5c627027a08e253b2
albator2002 <albator2002@gmail.com>
2016-09-24 10:20:44 -0400

    Fix login depedency

--- Jobine/app/login.ts
+++ Jobine/app/login.ts
@@ -3,22 +3,17 @@
  */
 // login.ts
 import {Component} from '@angular/core';
-import { FormBuilder, Validators, ControlGroup, NgIf} from '@angular/common';
+import {  NgIf} from '@angular/common';
 import {Router} from '@angular/router';
-import { PolymerElement } from '@vaadin/angular2-polymer';
+//import { PolymerElement } from '@vaadin/angular2-polymer';
 import {Authentication} from './authentication';
 
 @Component({
     selector: 'login',
-    directives: [
 
-        NgIf ,
-        PolymerElement('paper-input'),
-        PolymerElement('paper-button'),
-    ],
-    providers: [FormBuilder],
+
     template: `
-    <form [ngFormModel]="form" (submit)="onSubmit(form.value)">
+    <form  >
       <div *ngIf="error">Check your user name or password</div>
       <div>
        <paper-input id="username" name="username" [(value)]="_username" error-message="Invalide" label="Username"></paper-input>
@@ -27,24 +22,25 @@ import {Authentication} from './authentication';
         <paper-input id="password" name="password" [(value)]="_password" type="password"></paper-input>
       </div>
       <div class="form-group">
-        <paper-button raised on-click="submitForm()"   >submit</paper-button>
+        <paper-button raised on-click="login()"   >submit</paper-button>
       </div>
     </form>
   `
 })
 export class Login {
-    form: ControlGroup;
+
     error: boolean = false;
-    _username:string;
-    _password:string;
-    constructor(fb: FormBuilder, public auth: Authentication, public router: Router) {
-        this.form = fb.group({
-            username:  ['', Validators.required],
-            password:  ['', Validators.required]
-        });
+    _username:string = "";
+    _password:string = "";
+
+    constructor( private auth: Authentication,private router:Router ) {
+       // this.form = fb.group({
+       //     username:  ['', Validators.required],
+       //     password:  ['', Validators.required]
+       // });
     }
 
-    submitForm(value: any) {
+    login() {
         //var username = this.form.controls['username'].value;
         //var password = this.form.controls['password'].value;
         this.auth.login(this._username, this._password)
@@ -52,7 +48,8 @@ export class Login {
                 (token: any) => {
                     var btnLogin = <HTMLElement>document.querySelector('#btnLogin');
                     btnLogin.hidden = true;
-                    this.router.navigate(['../Menu'])},
+                    this.router.navigate(['../Menu'])
+                },
                         () => { this.error = true; }
 
             );
